651cfb552eae8d53efab9c838d53cf529cfd7b2b
Max Clifford <max.clifford@johnoconner.co.uk>
2020-02-15 12:09:53 +0000

    Add project files.

---
+++ Feedback/Result.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Feedback
+{
+    public class Result<T>
+    {
+        private readonly bool success;
+        private readonly T value;
+        private readonly string message;
+        private readonly int code;
+
+        public bool Success => success;
+
+        public T Value => value;
+
+        public string Message => message;
+
+        public int Code => code;
+
+        public Result(bool success, T value, string message, int code)
+        {
+            this.success = success;
+            this.value = value;
+            this.message = message;
+            this.code = code;
+        }
+
+        public static Result<T> CreateSucces(T value, string message = null, int code = 0)
+        {
+            return new Result<T>(true, value, message, code);
+        }
+
+        public static Result<T> CreateFailure(string message, int code = 0)
+        {
+            return new Result<T>(false, default(T), message, code);
+        }
+
+        public static implicit operator T(Result<T> result)
+        {
+            return result.Value;
+            // if (result.Success)
+            // else 
+            //    return default(T);
+        }
+    }
+}
